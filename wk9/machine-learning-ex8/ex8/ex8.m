d53ff4d543af8e0d998beeca14545b96cde349ad
Raoul <raoul.biagioni@ie.fujitsu.com>
2017-11-27 23:18:52 +0000

    dfds

--- wk9/machine-learning-ex8/ex8/ex8.m
+++ wk9/machine-learning-ex8/ex8/ex8.m
@@ -34,10 +34,10 @@ fprintf('Visualizing example dataset for outlier detection.\n\n');
 load('ex8data1.mat');
 
 %  Visualize the example dataset
-plot(X(:, 1), X(:, 2), 'bx');
-axis([0 30 0 30]);
-xlabel('Latency (ms)');
-ylabel('Throughput (mb/s)');
+%  plot(X(:, 1), X(:, 2), 'bx');
+%  axis([0 30 0 30]);
+%  xlabel('Latency (ms)');
+%  ylabel('Throughput (mb/s)');
 
 fprintf('Program paused. Press enter to continue.\n');
 pause
@@ -63,9 +63,9 @@ fprintf('Visualizing Gaussian fit.\n\n');
 p = multivariateGaussian(X, mu, sigma2);
 
 %  Visualize the fit
-visualizeFit(X,  mu, sigma2);
-xlabel('Latency (ms)');
-ylabel('Throughput (mb/s)');
+%  visualizeFit(X,  mu, sigma2);
+%  xlabel('Latency (ms)');
+%  ylabel('Throughput (mb/s)');
 
 fprintf('Program paused. Press enter to continue.\n');
 pause;
@@ -77,4 +77,24 @@ pause;
 %
 
 pval = multivariateGaussian(Xval, mu, sigma2);
+
+[epsilon F1] = selectThreshold(yval, pval) ;
+%[epsilon F1] = selectThreshold([1 0 0 1 1]', [0.1 0.2 0.3 0.4 0.5]')
+
+
+fprintf('Best epsilon found using cross-validation: %e\n', epsilon);
+fprintf('Best F1 on Cross Validation Set:  %f\n', F1);
+fprintf('   (you should see a value epsilon of about 8.99e-05)\n');
+fprintf('   (you should see a Best F1 value of  0.875000)\n\n');
+
+%  Find the outliers in the training set and plot the
+outliers = find(p < epsilon);
+
+%  Draw a red circle around those outliers
+hold on
+plot(X(outliers, 1), X(outliers, 2), 'ro', 'LineWidth', 2, 'MarkerSize', 10);
+hold off
+
+fprintf('Program paused. Press enter to continue.\n');
+pause;
 
